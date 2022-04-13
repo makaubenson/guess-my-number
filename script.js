0692c66fee481c77918b0ae173373c94619e08d5
unknown <bensonmakau2000@gmail.com>
2022-04-13 15:39:56 +0300

    getting value of an html input tag|

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 console.log(document.querySelector('.message').textContent);
 // Setting new textContent
 document.querySelector('.message').textContent = '🎉 Correct Number';
+// console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
