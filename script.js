014b39821e2324ab2d27735ec0a45f50a13cc50d
unknown <bensonmakau2000@gmail.com>
2022-04-13 18:52:16 +0300

    adding again reset functionality

--- script.js
+++ script.js
@@ -12,11 +12,9 @@ console.log(document.querySelector('.guess').value); //getting value of the inpu
 */
 // Handling Click Events
 //Generating a random secret number between 0 and 20
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
-//console.log(`Secret Number is ${secretNumber}`);
-
+console.log(secretNumber);
 document.querySelector('.check').addEventListener('click', function () {
   //listening for a click event and executing a function
   //   console.log(document.querySelector('.guess').value);
@@ -28,6 +26,9 @@ document.querySelector('.check').addEventListener('click', function () {
     //when the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
@@ -55,4 +56,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start guessing...';
+  secretNumber = Math.trunc(Math.random() * 20) + 1; //generate new secret number
+  console.log(secretNumber, `New Secret Number`); //log the secret number
+  //   document.querySelector('.score').textContent = 20;
+  score = 20; //reset score to 20
+  document.querySelector('.score').textContent = score; //manipulate DOM to show the new score
+  document.querySelector('body').style.backgroundColor = '#222'; //change background color of the body
+  document.querySelector('.number').style.width = '15rem'; //change a html div
+  document.querySelector('.number').textContent = '?'; //change textContent of a html tag
+  document.querySelector('.guess').value = ''; // reset input value to 0
 });
