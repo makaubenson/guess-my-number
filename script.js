1f00e074f724af54cf63ebc9911e69bcaf19c97d
unknown <bensonmakau2000@gmail.com>
2022-04-13 16:25:20 +0300

    manipulate DOM if value matches

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ console.log(document.querySelector('.guess').value); //getting value of the inpu
 */
 // Handling Click Events
 //Generating a random secret number between 0 and 20
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
-console.log(`Random number is ${number}`);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(`Secret Number is ${secretNumber}`);
 
 document.querySelector('.check').addEventListener('click', function () {
   //   console.log(document.querySelector('.guess').value);
@@ -22,5 +22,7 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
   }
 });
